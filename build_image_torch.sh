34720f7b0fc43d8c5084b8cf3e0362fdd67acd48
Jim Thompson <jimthompson5802@gmail.com>
2021-10-23 14:42:56 -0400

    update to torch 1.10.0 and related packages

--- build_image_torch.sh
+++ build_image_torch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 python_version=${1:-3.8}
-torch_version=${2:-1.9.0}
-torch_vision_version=${3:-0.10.0}
-torch_audio_version=${4:-0.9.0}
+torch_version=${2:-1.10.0}
+torch_vision_version=${3:-0.11.1}
+torch_audio_version=${4:-0.10.0}
 linux_distro=${5:-buster}
 
 # other valid value is 'plain'
@@ -13,7 +13,7 @@ progress=${6:-auto}
   --build-arg PYTHON_VERSION=${python_version} \
   --build-arg TORCH_VERSION=${torch_version} \
   --build-arg TORCH_VISION_VERSION=${torch_vision_version} \
-  --build-arg TORCH_ADUIO_VERSION=${torch_aduio_version} \
+  --build-arg TORCH_AUDIO_VERSION=${torch_audio_version} \
   --build-arg LINUX_DISTRO=${linux_distro} \
 	-t ludwig_mydev:py${python_version}-torch${torch_version} \
 	-f ./Dockerfile_torch ../ludwig
