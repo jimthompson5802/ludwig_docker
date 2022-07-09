1b65ac3439728949a3a1ab414d6d1d38306169b1
Jim Thompson <jimthompson5802@gmail.com>
2022-07-09 19:32:50 -0400

    update tensorflow image build

--- build_image_tf.sh
+++ build_image_tf.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 python_version=${1:-3.7}
-tensorflow_version=${2:-2.5.0}
+tensorflow_version=${2:-2.6.5}
 linux_distro=${3:-buster}
 
 # other valid value is 'plain'
@@ -11,8 +11,8 @@ docker build --progress ${progress} \
   --build-arg PYTHON_VERSION=${python_version} \
   --build-arg TENSORFLOW_VERSION=${tensorflow_version} \
   --build-arg LINUX_DISTRO=${linux_distro} \
-	-t ludwig_mydev:py${python_version}-tf${tensorflow_version} \
-	-f ./Dockerfile_mydev ../ludwig
+	-t tensorflow:py${python_version}-tf${tensorflow_version} \
+	-f ./Dockerfile_tf .
 
 
 
