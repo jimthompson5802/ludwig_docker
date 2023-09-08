af15dcb02655d503645bf43379fcafb4cebce702
Jim Thompson <jimthompson5802@gmail.com>
2023-09-08 18:21:55 -0400

    add lugwig nightly image

---
+++ build_image_ludwig_ray_nightly.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Build development image using Ludwig official image as base
+
+# base image tag
+tag=${1:-nightly}
+
+# other valid value is 'plain'
+progress=${2:-auto}
+
+docker build --progress ${progress} \
+  --build-arg BASE_IMAGE_TAG=${tag} \
+	-t ludwig-ray:${tag} \
+	-f ./Dockerfile_ludwig_ray_nightly .
+
